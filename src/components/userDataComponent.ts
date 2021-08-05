3afa13e56af4ad8b52dd08445b0035ffa2014fcb
Quan Nguyen <minhquan0902@gmail.com>
2021-08-05 22:37:16 +0800

    Finished Deployed FrontEnd

--- src/components/userDataComponent.ts
+++ src/components/userDataComponent.ts
@@ -2,7 +2,7 @@ import { useListUserQuery } from "../generated/graphql";
 
 export default function mappedUser() {
   const [{ data }] = useListUserQuery();
-  const mappedUser = data.listUser.map((v) => {
+  const mappedUser = data?.listUser?.map((v) => {
     return {
       id: v._id,
       username: v.username,
